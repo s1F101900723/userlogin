73cd3a6d7fe372a6de637e7a96eeb155a7aa78d0
Yukina Ogasawara <s1f101900723@iniad.org>
2021-09-28 22:14:32 +0900

    Update app.js

--- app.js
+++ app.js
@@ -235,12 +235,12 @@
    * Initializes the app.
    */
   var initApp = function() {
-    /*
+    
     document.getElementById('sign-in-with-redirect').addEventListener(
         'click', signInWithRedirect);
     document.getElementById('sign-in-with-popup').addEventListener(
         'click', signInWithPopup);
-    */
+    
     document.getElementById('sign-out').addEventListener('click', function() {
       firebase.auth().signOut();
     });
